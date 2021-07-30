58137244f3eb2e10d7827a3cc70325bb2927d805
AcelisWeaven <AcelisWeaven@users.noreply.github.com>
2021-07-30 14:50:24 +0000

    Deploying to gh-pages from @ AcelisWeaven/domodesu@5aa8f6b31763573338638370291db148b37e6021 🚀

--- _nuxt/832f251.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{262:function(e,n,t){"use strict";t.r(n);var l=t(0).a.extend({}),c=t(60),component=Object(c.a)(l,(function(){var e=this,n=e.$createElement;return(e._self._c||n)("h1",[e._v(e._s(e.$t("welcome")))])}),[],!1,null,null,null);n.default=component.exports}}]);
