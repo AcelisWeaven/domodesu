37128d03036c2b2727d18eac817dd774573d23fb
AcelisWeaven <jeremy.graziani@gmail.com>
2021-07-30 12:05:28 +0200

    🔧 Changed eslint/stylelint configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,15 +2,24 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
-  extends: [
-    '@nuxtjs/eslint-config-typescript',
-    'plugin:nuxt/recommended',
-    'prettier'
-  ],
-  plugins: [
-  ],
-  // add your custom rules here
-  rules: {}
+  // 'airbnb-typescript' requires dev dependencies we don't need, this will be fixed in the future
+  // --> Packages: eslint-plugin-jsx-a11y / eslint-plugin-react@^7.20.3 / eslint-plugin-react-hooks
+  extends: ['airbnb-typescript', 'plugin:nuxt/recommended', 'prettier'],
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    project: './tsconfig.json',
+    extraFileExtensions: ['.vue'],
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  plugins: [],
+  // Add your custom rules here
+  rules: {},
+  settings: {
+    // Needed because of 'airbnb-typescript', see above comment
+    react: {
+      version: '999.999.999',
+    },
+  },
 }
--- stylelint.config.js
+++ stylelint.config.js
@@ -1,9 +1,10 @@
 module.exports = {
   extends: [
-    'stylelint-config-standard',
-    'stylelint-config-prettier'
+    'stylelint-config-recommended'
   ],
-  // add your custom config here
-  // https://stylelint.io/user-guide/configuration
+  /*
+   * Add your custom config here
+   * https://stylelint.io/user-guide/configuration
+   */
   rules: {}
 }
