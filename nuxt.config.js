b2dcbf8c7f0a1d9afea8dd37da0904a8040e7ddf
AcelisWeaven <jeremy.graziani@gmail.com>
2021-07-30 18:22:21 +0200

    🐛 Avoid favicon redirection

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,15 @@ export default {
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: `${
+          process.env.BASE_URL || 'http://localhost:3000/domodesu/'
+        }favicon.ico`,
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
