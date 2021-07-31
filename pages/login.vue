be34220426422f98cfe2715d2245dc2fc77c6bdd
AcelisWeaven <jeremy.graziani@gmail.com>
2021-07-31 10:31:35 +0200

    🐛 Mitigate login "translation-blink"

--- pages/login.vue
+++ pages/login.vue
@@ -1,23 +1,25 @@
 <template>
-  <div class="my-8 mx-16">
-    <h1>{{ $t('welcome_login') }}</h1>
-    <button
-      class="
-        bg-purple-500
-        hover:bg-purple-700
-        text-white
-        font-bold
-        py-2
-        px-4
-        my-4
-        rounded
-        cursor-pointer
-      "
-      @click="login"
-    >
-      {{ $t('connect_twitch') }}
-    </button>
-  </div>
+  <client-only>
+    <div class="my-8 mx-16">
+      <h1>{{ $t('welcome_login') }}</h1>
+      <button
+        class="
+          bg-purple-500
+          hover:bg-purple-700
+          text-white
+          font-bold
+          py-2
+          px-4
+          my-4
+          rounded
+          cursor-pointer
+        "
+        @click="login"
+      >
+        {{ $t('connect_twitch') }}
+      </button>
+    </div>
+  </client-only>
 </template>
 
 <script lang="ts">
