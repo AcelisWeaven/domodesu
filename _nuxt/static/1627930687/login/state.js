578ad30133a31ac2c1f439c8a7fb083187f7c1cf
AcelisWeaven <AcelisWeaven@users.noreply.github.com>
2021-08-02 19:22:06 +0000

    Deploying to gh-pages from @ AcelisWeaven/domodesu@2664099a36fb08d373daa73d1766582bd185a6f3 🚀

--- _nuxt/static/1627930687/login/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fdomodesu\u002F_nuxt\u002Fstatic\u002F1627930687",layout:"default",error:a,state:{auth:{user:a,loggedIn:false,strategy:"twitch","twitch.state":void 0}},serverRendered:true,routePath:"\u002Flogin",config:{_app:{basePath:"\u002Fdomodesu\u002F",assetsPath:"\u002Fdomodesu\u002F_nuxt\u002F",cdnURL:a}}}}(null));
