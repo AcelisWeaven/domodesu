6aec53835769e05eed63dbeb28c34ef0d6c874cf
AcelisWeaven <AcelisWeaven@users.noreply.github.com>
2021-08-03 15:39:26 +0000

    Deploying to gh-pages from @ AcelisWeaven/domodesu@95d1b45d6b0cdb80ec44d815e02aa4266474b02b 🚀

---
+++ _nuxt/static/1628005165/monitor/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fdomodesu\u002F_nuxt\u002Fstatic\u002F1628005165",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002Fmonitor",config:{_app:{basePath:"\u002Fdomodesu\u002F",assetsPath:"\u002Fdomodesu\u002F_nuxt\u002F",cdnURL:a}}}}(null));
